1f2353b73602a174ec47adc5985398d6ab29f51f
LucasFerruchi <lucasferruchi05@gmail.com>
2024-06-13 10:48:06 -0300

    uso de API. Promesas y asincronismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { detalleCard } from "./data/data.js";
+import CardApp from "./components/CardApp.jsx";
+import CarouselApp from "./components/CarouselApp.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <CarouselApp />
+      <div className="container">
+        <div className="row">
+          {detalleCard.map((item, index) => (
+            <CardApp item={item} key={index} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,88 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+//importar funcion obtenerClima
+import { obtenerClima } from "../helpers/obtener-clima";
+import "../css/navbar.css";
 
 const NavBar = () => {
+  //hooks - useState y useEffect
+
+  const [tiempo, setTiempo] = useState(null);
+
+  useEffect(() => {
+    clima();
+  }, []);
+
+  const clima = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const coords = pos.coords;
+      const lat = coords.latitude;
+      const long = coords.longitude;
+
+      // console.log(coords);
+
+      obtenerClima(lat, long)
+        .then((respuesta) => {
+          // const { main, weather } = respuesta;
+
+          setTiempo({
+            // temp: main.temp,
+            // clima: weather[0],
+            temp: respuesta.main.temp,
+            clima: respuesta.weather[0],
+          });
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+      <div className="fixed-top">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <i className="fa fa-ravelry" aria-hidden="true"></i>
+              97iRobots
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Inicio
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contacto
+                  </a>
+                </li>
+              </ul>
+              {tiempo && (
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${tiempo.clima.icon}@2x.png`}
+                    alt=""
+                    className="icon-tiempo"
+                  />
+                  <span>{Math.round(tiempo.temp)}°C</span>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </nav>
-      ;
+        </nav>
+      </div>
     </>
   );
 };
